422ace2c7fd5dd50c4521326e400799237257cbd
Abdelkabir Ouadoukou <152230716+khabzo@users.noreply.github.com>
2024-03-30 22:58:59 +0000

    Update File of CalcAgeApp

--- app.cpp
+++ app.cpp
@@ -20,9 +20,13 @@ int main()
 
   int age_in_days = age * 365;
   int age_in_hours = age_in_days * 24;
+  int age_in_min = age_in_hours * 60;
+  int age_in_s = age_in_min * 60;
 
   cout << "Age In Days Is: " << age_in_days << " Days\n";
   cout << "Age In Hours Is: " << age_in_hours << " Hours\n";
+  cout << "Age In min Is: " << age_in_min << " Min\n";
+  cout << "Age In s Is: " << age_in_s << " s\n";
 
   return 0;
 }
